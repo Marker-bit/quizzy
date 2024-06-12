8ceebd4d42a9228aeab67f504f2c6d0a4e0fb6dc
Marker-bit <maarrrkkkk@ya.ru>
2024-06-12 10:53:54 +0300

    update login page

--- src/app/(auth)/login/email-sent/page.tsx
+++ src/app/(auth)/login/email-sent/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { env } from "@/env";
 import { MailCheck } from "lucide-react";
 
 export default function LoginPage() {
+  const email = env.EMAIL_FROM;
   return (
     <div className="flex justify-center pt-[20vh]">
       <div className="flex flex-col gap-2 rounded-xl border bg-white p-4 shadow-md">
@@ -11,8 +13,7 @@ export default function LoginPage() {
           Письмо отправлено
         </h1>
         <p>
-          Проверьте свою почту, там должно быть письмо от{" "}
-          <code>onboarding@markerbit.ru</code>.
+          Проверьте свою почту, там должно быть письмо от <code>{email}</code>.
         </p>
         <p>Письмо могло попасть в папку &quot;Спам&quot;.</p>
         <p>Вы можете закрыть эту вкладку.</p>
